2309f38eddf0aa527b0090e418e325d5185c785d
nicosabo <nicosabo@hotmail.com>
2022-08-31 01:18:20 -0300

    primer subida

---
+++ src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { AiOutlineShoppingCart } from 'react-icons/ai';
+
+export const CartWidget = () => {
+  return (
+    <>
+    <AiOutlineShoppingCart className='icon'/>
+    </>
+  )
+}
